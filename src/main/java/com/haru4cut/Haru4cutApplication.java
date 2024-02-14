d23e16b53fcd1f5ed996b0d866729d9408e3a61e
JUN <as2686@naver.com>
2024-02-14 18:29:08 +0900

    initial commit

---
+++ src/main/java/com/haru4cut/Haru4cutApplication.java
@@ -0,0 +1,13 @@
+package com.haru4cut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Haru4cutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Haru4cutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haru4cut/Haru4cutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haru4cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Haru4cutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

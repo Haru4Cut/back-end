21a415687e293cf6f1d17ce3528bf9a7d292f3dd
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-02-23 02:31:02 +0900

    test: 일기 수정 기능 테스트 코드 구현

--- src/test/java/com/haru4cut/diary/DiaryServiceTest.java
+++ src/test/java/com/haru4cut/diary/DiaryServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -42,4 +43,29 @@ class DiaryServiceTest {
         assertNotNull(diaryId);
         assertTrue(diaryId > 0);
     }
+
+    @Test
+    @DisplayName("일기 수정 테스트 - text만 수정 가능")
+    void updateDiaryTest(){
+        //given
+        Long userId = 1L;
+        Users user = new Users("wnstj1128@naver.com", "wnstj1819");
+        userRepository.save(user);
+
+        DiaryRequestDto requestDto = new DiaryRequestDto(user, 4, Arrays.asList(
+                "http://localhost/image/image1.png",
+                "http://localhost/image/image2.png",
+                "http://localhost/image/image3.png",
+                "http://localhost/image/image4.png"
+        ), "오늘은 친구들이랑 밥을 먹었구여 아주 행복합니다앙");
+
+        Long diaryId = diaryService.createDiary(userId, requestDto);
+        String text = "바뀌었나요?";
+        //when
+        diaryService.updateDiary(diaryId, text);
+        //then
+        Optional<Diary> diary = diaryRepository.findById(diaryId);
+        assertEquals(text, diary.get().getText());
+    }
+
 }

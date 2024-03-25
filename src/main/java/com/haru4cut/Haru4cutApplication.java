b63162e61591e7930ad3512bd78856653d4478db
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-26 00:20:02 +0900

    bug : CORS 에러 수정

--- src/main/java/com/haru4cut/Haru4cutApplication.java
+++ src/main/java/com/haru4cut/Haru4cutApplication.java
@@ -2,6 +2,9 @@ package com.haru4cut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Haru4cutApplication {
@@ -10,4 +13,13 @@ public class Haru4cutApplication {
 		SpringApplication.run(Haru4cutApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }

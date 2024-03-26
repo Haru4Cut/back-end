28fe4eb96b3c4a989d73d1a3ea841b8e744d670d
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-27 01:05:07 +0900

    bug : CORS 에러 수정 - webfilter 추가

--- src/main/java/com/haru4cut/Haru4cutApplication.java
+++ src/main/java/com/haru4cut/Haru4cutApplication.java
@@ -2,11 +2,13 @@ package com.haru4cut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Haru4cutApplication {
 
 	public static void main(String[] args) {

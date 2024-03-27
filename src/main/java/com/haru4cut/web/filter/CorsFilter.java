125117bf772457358dac0c9fc9737f58e6218b34
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-27 15:19:02 +0900

    bug : CORS 에러 수정 - CorsFilter 수정

--- src/main/java/com/haru4cut/web/filter/CorsFilter.java
+++ src/main/java/com/haru4cut/web/filter/CorsFilter.java
@@ -12,11 +12,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    // Filter Config 작성하니까 이거 에러남 -> 확인 필요
+    // Filter Config 작성하니까 이거 에러남 -> 확인 필성
 //    @Override
 //    public void init(FilterConfig filterConfig) throws ServletException {
 //

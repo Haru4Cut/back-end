d1db1f8dff8b8cf386ca20aec2f43a33098fcd22
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-27 15:10:36 +0900

    bug : CORS 에러 수정 - CorsFilter 수정 - FilterConfig 작성

---
+++ src/main/java/com/haru4cut/web/filter/FilterConfig.java
@@ -0,0 +1,18 @@
+package com.haru4cut.web.filter;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<Filter> corsFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new CorsFilter());
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+}

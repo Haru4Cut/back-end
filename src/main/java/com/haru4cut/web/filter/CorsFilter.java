29753989f200f401c67afd99e1c0c965d6f621a4
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-03-29 00:23:10 +0900

    bug : CORS 에러 수정 - WebConfig 파일 작성

--- src/main/java/com/haru4cut/web/filter/CorsFilter.java
+++ src/main/java/com/haru4cut/web/filter/CorsFilter.java
@@ -1,46 +1,46 @@
-package com.haru4cut.web.filter;
-
-
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.Order;
-import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-
-    // Filter Config 작성하니까 이거 에러남 -> 확인 필성
+//package com.haru4cut.web.filter;
+//
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.junit.jupiter.api.Order;
+//import org.springframework.core.Ordered;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CorsFilter implements Filter {
+//
+//    // Filter Config 작성하니까 이거 에러남 -> 확인 필성
+////    @Override
+////    public void init(FilterConfig filterConfig) throws ServletException {
+////
+////    }
+//
 //    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        HttpServletResponse response = (HttpServletResponse) res;
+//
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//        response.setHeader("Access-Control-Allow-Credentials", "true");
+//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//
+//        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        }else {
+//            chain.doFilter(req, res);
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
 //
 //    }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        }else {
-            chain.doFilter(req, res);
-        }
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//}

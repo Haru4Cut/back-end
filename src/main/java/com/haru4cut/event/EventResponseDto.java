807c3e117260b8bdce160f1a286d09f964420852
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2024-04-05 17:36:10 +0900

    refactor: 좋아요 누른 사진 받을 때 날짜도 같이 전송

--- src/main/java/com/haru4cut/event/EventResponseDto.java
+++ src/main/java/com/haru4cut/event/EventResponseDto.java
@@ -7,8 +7,10 @@ import java.util.List;
 @Data
 public class EventResponseDto {
     List<String> imgLinks;
-    public EventResponseDto(List<String> imgLinks) {
+    String date;
+    public EventResponseDto(List<String> imgLinks, String date) {
         this.imgLinks = imgLinks;
+        this.date = date;
     }
 
 }
